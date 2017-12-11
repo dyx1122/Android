a890b6a859267b92be647b726374c7908a2d4248
Yinxue <dyxue1122@gmail.com>
2017-12-11 14:48:07 -0800

    Read/write json

--- TestJSON/app/src/main/java/com/example/jdong/testjson/MainActivity.java
+++ TestJSON/app/src/main/java/com/example/jdong/testjson/MainActivity.java
@@ -18,30 +18,69 @@ public class MainActivity extends AppCompatActivity {
   protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+//    try {
+//      InputStreamReader isr = new InputStreamReader(getAssets().open("test.json"), "UTF-8");
+//      BufferedReader br = new BufferedReader(isr);
+//      String line;
+//      StringBuilder builder = new StringBuilder();
+//      while ((line = br.readLine()) != null) {
+//        builder.append(line);
+//      }
+//      br.close();
+//      isr.close();
+//      JSONObject root = new JSONObject(builder.toString());
+//      System.out.println("cat=" + root.getString("cat"));
+//      JSONArray array = root.getJSONArray("languages");
+//      for (int i = 0; i < array.length(); i++) {
+//        JSONObject lan = array.getJSONObject(i);
+//        System.out.println("---------------");
+//        System.out.println("id="+ lan.getInt("id"));
+//        System.out.println("name="+ lan.getString("name"));
+//        System.out.println("ide="+ lan.getString("ide"));
+//      }
+//    } catch (UnsupportedEncodingException e) {
+//      e.printStackTrace();
+//    } catch (IOException e) {
+//      e.printStackTrace();
+//    } catch (JSONException e) {
+//      e.printStackTrace();
+//    }
+
+
+//    {
+//      "languages":[
+//      {"id":1, "ide":"Eclipse", "name":"Java"},
+//      {"id":2, "ide":"XCode", "name":"Swift"},
+//      {"id":3, "ide":"Visual Studio", "name":"C#"}
+//	],
+//      "cat":"it"
+//    }
+
+    JSONObject root = new JSONObject();
     try {
-      InputStreamReader isr = new InputStreamReader(getAssets().open("test.json"), "UTF-8");
-      BufferedReader br = new BufferedReader(isr);
-      String line;
-      StringBuilder builder = new StringBuilder();
-      while ((line = br.readLine()) != null) {
-        builder.append(line);
-      }
-      br.close();
-      isr.close();
-      JSONObject root = new JSONObject(builder.toString());
-      System.out.println("cat=" + root.getString("cat"));
-      JSONArray array = root.getJSONArray("languages");
-      for (int i = 0; i < array.length(); i++) {
-        JSONObject lan = array.getJSONObject(i);
-        System.out.println("---------------");
-        System.out.println("id="+ lan.getInt("id"));
-        System.out.println("name="+ lan.getString("name"));
-        System.out.println("ide="+ lan.getString("ide"));
-      }
-    } catch (UnsupportedEncodingException e) {
-      e.printStackTrace();
-    } catch (IOException e) {
-      e.printStackTrace();
+      root.put("cat", "it");
+      JSONObject lan1 = new JSONObject();
+      lan1.put("id", 1);
+      lan1.put("ide", "Eclipse");
+      lan1.put("name", "Java");
+
+      JSONObject lan2 = new JSONObject();
+      lan2.put("id", 2);
+      lan2.put("ide", "Xcode");
+      lan2.put("name", "Swift");
+
+      JSONObject lan3 = new JSONObject();
+      lan3.put("id", 3);
+      lan3.put("ide", "Visual Studio");
+      lan3.put("name", "C#");
+
+      JSONArray array = new JSONArray();
+      array.put(lan1);
+      array.put(lan2);
+      array.put(lan3);
+
+      root.put("languages", array);
+      System.out.println(root.toString());
     } catch (JSONException e) {
       e.printStackTrace();
     }
